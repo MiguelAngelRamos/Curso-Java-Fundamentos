aeef2a705c9e3f754adbb4bd7e2af49cebf1ae61
Miguel Ramos <return.infox@gmail.com>
2025-06-28 11:02:42 -0400

    first commit

---
+++ ejercicio-1/casouso/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double total; // valor por defecto double es 0.0
+        System.out.println("Ventas diarias: ");
+
+    }
+}
+// 
+/* */
